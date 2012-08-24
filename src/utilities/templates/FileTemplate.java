fe610597b157e8660fcbcfd5414ea23823d0a312
Tobias Ramforth <tobias@ramforth.com>
2012-08-24 20:03:07 +0000

    modified filetemplate - working
    git-svn-id: svn://irgsmirx.dyndns.org/Utilities/trunk@40 6dbdd020-3af3-495c-8cb0-d40fca5d4734

--- src/utilities/templates/FileTemplate.java
+++ src/utilities/templates/FileTemplate.java
@@ -6,8 +6,14 @@ package utilities.templates;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,7 +22,7 @@ import java.util.logging.Logger;
  * @author Tobias Ramforth <tobias.ramforth at tu-dortmund.de>
  */
 public class FileTemplate extends AbstractTemplate {
-  
+
   private File template;
   
   public FileTemplate(File file) {
@@ -38,29 +44,145 @@ public class FileTemplate extends AbstractTemplate {
     super(placeholderBeginTag, placeholderEndTag, escapeCharacter);
     this.template = new File(filePath);
   }
-
   
-  private InputStreamTemplate createInputStreamTemplate() {
-    try {
-      FileInputStream fileInputStream = new FileInputStream(template);
-      InputStreamTemplate inputStreamTemplate = new InputStreamTemplate(fileInputStream);
-      return inputStreamTemplate;
-    } catch (FileNotFoundException ex) {
-      Logger.getLogger(FileTemplate.class.getName()).log(Level.SEVERE, null, ex);
-      throw new RuntimeException(ex);
-    }
+  private void renderTo(ICharRenderer renderer) {
+    if (template != null && !placeholderMap.isEmpty()) {
+			StringBuilder sb = new StringBuilder();
+
+			boolean inPlaceholder = false;
+			boolean inIndexer = false;
+
+			Field currentField = null;
+			String currentPlaceholderKey = null;
+			Object currentValue = null;
+
+			try (Reader reader = new InputStreamReader(new FileInputStream(template))) {
+        int templateCharacter;
+        try {
+          while ((templateCharacter = reader.read()) != -1) {
+            if (templateCharacter == placeholderBeginTag) {
+              if (inPlaceholder) {
+                sb.append((char)templateCharacter);
+              } else {
+                inPlaceholder = true;
+              }
+            } else if (templateCharacter == placeholderEndTag) {
+              if (sb.length() > 0) {
+                String propertyName = sb.toString();
+                if (currentValue == null) {
+                  currentValue = placeholderMap.get(propertyName);
+                  renderer.render(currentValue);
+                } else {
+                  Field field = ReflectionUtilities.findFieldIn(currentValue.getClass(), propertyName);
+                  Object fieldValue = ReflectionUtilities.getFieldValueFrom(field, currentValue);
+                  currentPlaceholderKey = null;
+                  currentValue = null;
+                  if (fieldValue == null) {
+                    sb.append("NULL");
+                  } else {
+                    sb.append(fieldValue);
+                  }
+                }
+
+                sb.setLength(0);
+              }
+              inPlaceholder = false;
+              inIndexer = false;
+            } else if (templateCharacter == '[') {
+              String propertyName = sb.toString();
+              if (inPlaceholder) {
+                currentField = ReflectionUtilities.findFieldIn(currentValue.getClass(), propertyName);
+                currentValue = ReflectionUtilities.getFieldValueFrom(currentField, currentValue);
+                sb.setLength(0);
+
+                inIndexer = true;
+              } else {
+                renderer.render((char)templateCharacter);
+              }
+            } else if (templateCharacter == ']') {
+              if (inPlaceholder) {
+                if (inIndexer) {
+                  String propertyName = sb.toString();
+                  try {
+                    int indexValue = Integer.parseInt(propertyName);
+
+                    if (currentValue.getClass().isArray()) {
+                      currentValue = Array.get(currentValue, indexValue);
+                    } else if (currentValue instanceof List<?>) {
+                      currentValue = ((List<?>) currentValue).get(indexValue);
+                    } else if (currentValue instanceof Map<?, ?>) {
+                      currentValue = ((Map<?, ?>) currentValue).get(indexValue);
+                    }
+                  } catch (NumberFormatException nfex) {
+                    if (currentValue instanceof Map<?, ?>) {
+                      currentValue = ((Map<?, ?>) currentValue).get(propertyName);
+                    }
+                  }
+
+                  sb.setLength(0);
+
+                  inIndexer = false;
+                }
+              } else {
+                renderer.render((char)templateCharacter);
+              }
+            } else if (templateCharacter == '.') {
+              if (inPlaceholder) {
+                if (sb.length() > 0) {
+                  String propertyName = sb.toString();
+                  if (currentValue == null) {
+                    currentValue = placeholderMap.containsKey(currentPlaceholderKey);
+                    currentPlaceholderKey = propertyName;
+                  } else {
+                    Field field = ReflectionUtilities.findFieldIn(currentValue.getClass(), propertyName);
+                    currentValue = ReflectionUtilities.getFieldValueFrom(field, currentValue);
+                    currentPlaceholderKey = propertyName;
+                  }
+                  sb.setLength(0);
+                }
+              } else {
+                renderer.render((char)templateCharacter);
+              }
+            } else {
+              if (inPlaceholder) {
+                sb.append((char)templateCharacter);
+              } else {
+                renderer.render((char)templateCharacter);
+              }
+            }
+          }
+        } catch (IOException ioex) {
+          Logger.getLogger(StringTemplate.class.getName()).log(Level.SEVERE, null, ioex);
+          throw new utilities.exceptions.IOException(ioex);
+        }
+
+        try {
+          reader.close();
+        } catch (IOException ioex) {
+          Logger.getLogger(InputStreamTemplate.class.getName()).log(Level.SEVERE, null, ioex);
+          throw new utilities.exceptions.IOException(ioex);
+        }
+      } catch (IOException ioex) {
+        Logger.getLogger(InputStreamTemplate.class.getName()).log(Level.SEVERE, null, ioex);
+        throw new utilities.exceptions.IOException(ioex);
+      }
+		}
   }
   
   @Override
   public void renderTo(OutputStream outputStream) {
-    createInputStreamTemplate().renderTo(outputStream);
+		ICharRenderer renderer = new OutputStreamRenderer(outputStream);
+		renderTo(renderer);
   }
 
   @Override
   public String render() {
-    return createInputStreamTemplate().render();
+		StringBuilder renderBuilder = new StringBuilder();
+
+		ICharRenderer renderer = new StringBuilderRenderer(renderBuilder);
+		renderTo(renderer);
+    
+		return renderBuilder.toString();
   }
-  
-  
   
 }
