2d5d0650fb6aeb4031364fe4d1ac3e8b999bff7d
Tobias Ramforth <tobias@ramforth.com>
2012-09-06 21:04:19 +0000

    - bugfix (body parsing) - whitelist / blacklist
    git-svn-id: svn://irgsmirx.dyndns.org/Utilities/trunk@52 6dbdd020-3af3-495c-8cb0-d40fca5d4734

---
+++ src/utilities/path/NegatingFileFilter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities.path;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ *
+ * @author Tobias Ramforth <tobias.ramforth at tu-dortmund.de>
+ */
+public class NegatingFileFilter implements FileFilter {
+  
+  private FileFilter child;
+  
+  public NegatingFileFilter(FileFilter child) {
+    this.child = child;
+  }
+
+  @Override
+  public boolean accept(File pathname) {
+    return !child.accept(pathname);
+  }
+  
+}
