a063466ed1cd742d88fec8b032440b4cb94f55e7
Tobias Ramforth <tobias@ramforth.com>
2013-02-23 16:02:58 +0100

    added notequalexpression

---
+++ src/main/java/com/ramforth/utilities/expressions/implementation/NotEqualExpression.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ramforth.utilities.expressions.implementation;
+
+import com.ramforth.utilities.expressions.interfaces.INotEqualExpression;
+import com.ramforth.utilities.expressions.interfaces.IExpression;
+
+/**
+ *
+ * @author Tobias Ramforth <tobias.ramforth at tu-dortmund.de>
+ */
+public class NotEqualExpression extends AbstractConditionalExpression implements INotEqualExpression {
+
+    public NotEqualExpression(IExpression leftChild, IExpression rightChild) {
+        super(leftChild, rightChild);
+    }
+}
